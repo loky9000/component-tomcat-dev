085e09d9d46f5731b4feab9b0347853746553ae6
Anton Panasenko <anton.panasenko@gmail.com>
2013-12-25 18:46:42 +0200

    Added testing environment

--- test/test.py
+++ test/test.py
@@ -1,9 +1,55 @@
 import os
 
 from test_runner import BaseComponentTestCase
-from qubell.api.private.testing import instance, workflow, values
+from qubell.api.private.testing import instance, environment, workflow, values
 
-
+@environment({
+    "default": {},
+    "AmazonEC2_CentOS_63": {
+        "policies": [{
+            "action": "provisionVms",
+            "parameter": "imageId",
+            "value": "us-east-1/ami-eb6b0182"
+        }, {
+            "action": "provisionVms",
+            "parameter": "vmIdentity",
+            "value": "root"
+        }]
+    },
+    "AmazonEC2_CentOS_53": {
+        "policies": [{
+            "action": "provisionVms",
+            "parameter": "imageId",
+            "value": "us-east-1/ami-beda31d7"
+        }, {
+            "action": "provisionVms",
+            "parameter": "vmIdentity",
+            "value": "root"
+        }]
+    },
+    "AmazonEC2_Ubuntu_1204": {
+        "policies": [{
+            "action": "provisionVms",
+            "parameter": "imageId",
+            "value": "us-east-1/ami-d0f89fb9"
+        }, {
+            "action": "provisionVms",
+            "parameter": "vmIdentity",
+            "value": "ubuntu"
+        }]
+    },
+    "AmazonEC2_Ubuntu_1004": {
+        "policies": [{
+            "action": "provisionVms",
+            "parameter": "imageId",
+            "value": "us-east-1/ami-0fac7566"
+        }, {
+            "action": "provisionVms",
+            "parameter": "vmIdentity",
+            "value": "ubuntu"
+        }]
+    }
+})
 class TomcatDevComponentTestCase(BaseComponentTestCase):
     name = "component-tomcat-dev"
     apps = [{
