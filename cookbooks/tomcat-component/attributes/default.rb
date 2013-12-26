5e1d73e683c9aac1fd87c7f44d8e1e8764afca81
Anton Butovich <abutovich@gmail.com>
2013-12-26 19:04:53 +0200

    recipe deploy libs added

--- cookbooks/tomcat-component/attributes/default.rb
+++ cookbooks/tomcat-component/attributes/default.rb
@@ -9,3 +9,5 @@ default['maven']['m2_home'] = '/usr/local/maven'
 default['maven']['2']['version'] = "2.2.1"
 default['maven']['2']['url'] = "http://apache.mirrors.tds.net/maven/maven-2/#{node['maven']['2']['version']}/binaries/apache-maven-#{node['maven']['2']['version']}-bin.tar.gz"
 default['maven']['2']['checksum'] = "b9a36559486a862abfc7fb2064fd1429f20333caae95ac51215d06d72c02d376"
+
+default['tomcat-component']['lib_uri'] = ""
