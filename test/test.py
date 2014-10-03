741f6d7194be25e11a9b4bd31190bad647bcef4e
Artem Ivantsov <aivantsov@qubell.com>
2014-10-03 14:24:59 +0300

    test update tag version:1.0-33p

--- test/test.py
+++ test/test.py
@@ -5,50 +5,6 @@ from qubell.api.private.testing import instance, environment, workflow, values
 
 @environment({
     "default": {},
-    "AmazonEC2_CentOS_63": {
-        "policies": [{
-            "action": "provisionVms",
-            "parameter": "imageId",
-            "value": "us-east-1/ami-eb6b0182"
-        }, {
-            "action": "provisionVms",
-            "parameter": "vmIdentity",
-            "value": "root"
-        }]
-    },
-    "AmazonEC2_CentOS_53": {
-        "policies": [{
-            "action": "provisionVms",
-            "parameter": "imageId",
-            "value": "us-east-1/ami-beda31d7"
-        }, {
-            "action": "provisionVms",
-            "parameter": "vmIdentity",
-            "value": "root"
-        }]
-    },
-    "AmazonEC2_Ubuntu_1204": {
-        "policies": [{
-            "action": "provisionVms",
-            "parameter": "imageId",
-            "value": "us-east-1/ami-d0f89fb9"
-        }, {
-            "action": "provisionVms",
-            "parameter": "vmIdentity",
-            "value": "ubuntu"
-        }]
-    },
-    "AmazonEC2_Ubuntu_1004": {
-        "policies": [{
-            "action": "provisionVms",
-            "parameter": "imageId",
-            "value": "us-east-1/ami-0fac7566"
-        }, {
-            "action": "provisionVms",
-            "parameter": "vmIdentity",
-            "value": "ubuntu"
-        }]
-    }
 })
 class TomcatDevComponentTestCase(BaseComponentTestCase):
     name = "component-tomcat-dev"
@@ -67,7 +23,3 @@ class TomcatDevComponentTestCase(BaseComponentTestCase):
 
         assert result == 0
 
-    @instance(byApplication=name)
-    @workflow("tomcat.build-app", {"scm-provider": "git", "git-uri": "git://github.com/qubell/starter-java-web.git", "app-branch": "HEAD"})
-    def test_build(self, instance):
-        assert True
